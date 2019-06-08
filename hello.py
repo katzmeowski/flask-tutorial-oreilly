92be0a95675b6a999cfb43bf01852d93a8992a70
Kevin Jablonowski <kjazbo@gmail.com>
2019-06-08 18:18:41 -0400

    Add SQL support with Roles and Users models
    SQL database will be accessed via SQLAlchemy ORM, made accessible to
    Flask by Flask-SQLAlchemy extension. The instantiation of the database
    and the definition of the Roles and Users models have been set, but the
    database tables have not yet been created by the app.

--- hello.py
+++ hello.py
@@ -8,9 +8,19 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
+# Database
+import os
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 ##### WTFORM SECRET KEY #####
 # Using this for local development is okay, but never add a secret key to version control for anything that will be open to the internet
@@ -37,9 +47,29 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
+# WTForm form definitions
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+# DB model definitions
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
