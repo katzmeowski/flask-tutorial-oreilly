01dbd676c9cb9418606f24aafb482c9625b9e02f
Kevin Jablonowski <kjazbo@gmail.com>
2019-06-08 03:32:47 +0100

    Add web form support with Flask-WTF extension
    Flask-WTF provides helpers that allow Python to interact with the
    WTForm.js library. Flask-Bootstrap contains a handy base template
    that applies nice styles to WTForm objects.
    
    A simple test form was added to the root page of the app to
    demonstrate field validation and data submission via POST request.
    
    NOTE: The SECRET_KEY variable included is for learning purposes and
    convenience, but should never be used for an internet-facing
    deployment. Remove it from the source and recreate it as a randomly
    generated environment variable before deploymnet.

--- hello.py
+++ hello.py
@@ -3,13 +3,28 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
 
+# Forms and validation
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+##### WTFORM SECRET KEY #####
+# Using this for local development is okay, but never add a secret key to version control for anything that will be open to the internet
+# Change this key and store as an environment variable before deploying
+app.config['SECRET_KEY'] = 'do not use in prod or store in version control! create random environment variable instead'
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
 
 @app.route('/user/<name>')
 def user(name):
@@ -22,6 +37,10 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
